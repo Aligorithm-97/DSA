d4beae82e98a8b30c2e41677882f7e23b820ba88
Aligorithm-97 <atemurtas97@gmail.com>
2024-10-21 21:44:44 +0300

    aligorithm-subjects

---
+++ src/main/java/dsa/dsa/contents/Subjects.java
@@ -0,0 +1,23 @@
+package dsa.dsa.contents;
+
+public class Subjects {
+
+    private String arraysAndHashing;
+    private String twoPointers;
+    private String stack;
+    private String binarySearch;
+    private String slidingWindow;
+    private String LinkedList;
+    private String trees;
+    private String tries;
+    private String backTracking;
+    private String heapPriorityQueue;
+    private String graphs;
+    private String oneDimensionalDynamicProgramming;
+    private String twoDimensionalDynamicProgramming;
+    private String bitManipulation;
+    private String intervals;
+    private String greedy;
+    private String advancedGraphs;
+
+}
