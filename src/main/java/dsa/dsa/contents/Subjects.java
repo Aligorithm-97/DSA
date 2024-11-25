782ec627de8c9c2d9827390d46cc161a2c8f26b7
ali <atemurtas97@gmail.com>
2024-11-25 17:02:46 +0300

    aligorithm-trie

--- src/main/java/dsa/dsa/contents/Subjects.java
+++ src/main/java/dsa/dsa/contents/Subjects.java
@@ -166,6 +166,18 @@ Binary Heap
 
 Trie
 
+    A trie is a tree-based data structure that organizes information in a hierarchy.
+        Properties :
+            - It is typically used to store or search strings in a space and time efficient way.
+            - Any node in trie can store non repetitive multiple characters.
+            - Every node stores link of the next character of the string
+            - Every node keeps track of 'end of string'
+    Why
+        To solve many standary problems in efficient way
+            - Spelling checker
+            - Auto completion
+
+
 --------------------------------------------------------------
 
 Hashing
